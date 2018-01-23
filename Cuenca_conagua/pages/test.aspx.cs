fbf63530ca47dfad611e84748374211525581c71
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-01-23 13:41:04 -0600

    Agrega .gitignore adecuado al proyecto

---
+++ Cuenca_conagua/pages/test.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Cuenca_conagua.src.Utilidades;
+
+namespace Cuenca_conagua.pages
+{
+    public partial class test_aspx: System.Web.UI.Page
+    {
+        protected void btnExample_OnClick(object sender, EventArgs e)
+        {
+            Logger.AddToLog("BtnExample OnClick", true);
+        }
+    }
+}
