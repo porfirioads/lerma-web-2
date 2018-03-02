06aa9044312f287c3ee5302e547539f536bfe383
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-03-02 14:06:41 -0600

    Elimina antiguos docs de reglamentación

--- Cuenca_conagua/pages/reglamentacion.aspx.cs
+++ Cuenca_conagua/pages/reglamentacion.aspx.cs
@@ -1,11 +1,6 @@
 ﻿using Cuenca_conagua.src.Utilidades;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
 
 namespace Cuenca_conagua.pages
 {
